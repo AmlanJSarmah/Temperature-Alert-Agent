1f5b12f7c8648ed91cd7b3308d43a8d80096639a
A-J-Sarmah <ajsarmah@tutanota.com>
2023-10-09 15:46:58 +0530

    Added feature to select units

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
-from temperature.temperature import get_city_temperature
+from temperature.temperature import get_city_temperature, set_temperature_unit
 
 if __name__ == "__main__":
+    print("Welcome abroad ! We are pleased to welcome you to our temperature alert agent")
     city_name = input("Enter city name : ")
-    city_temperature = get_city_temperature(city_name)
+    temperature_unit = set_temperature_unit()
+    city_temperature = get_city_temperature(city_name, temperature_unit)
     if city_temperature is not None:
         print(f"The Temperature of {city_name} is {city_temperature}")
     else:
--- src/temperature/temperature.py
+++ src/temperature/temperature.py
@@ -3,7 +3,15 @@ import requests
 from dotenv import load_dotenv
 
 
-def get_city_temperature(city_name: str):
+def kelvin_to_celsius(temperature):
+    return temperature - 273.15
+
+
+def kelvin_to_fahrenheit(temperature):
+    return (temperature - 273.15) * 1.8 + 32
+
+
+def get_city_temperature(city_name, temperature_unit):
     load_dotenv()
     api_key = os.getenv("API_KEY")
     base_url = "https://api.openweathermap.org/data/2.5/weather?"
@@ -14,6 +22,23 @@ def get_city_temperature(city_name: str):
     if response_json["cod"] != "404":
         data = response_json["main"]
         temperature = data["temp"]
+        if temperature_unit == "C":
+            temperature = kelvin_to_celsius(temperature)
+        if temperature_unit == "F":
+            temperature = kelvin_to_fahrenheit(temperature)
         return temperature
     else:
         return None
+
+
+def set_temperature_unit():
+    unit = input("Enter C for Celsius, F for Fahrenheit and K for Kelvin. If you enter anything else we will put "
+                 "Celsius as default ")
+    if unit == "C":
+        return "C"
+    elif unit == "F":
+        return "F"
+    elif unit == "K":
+        return "K"
+    else:
+        return "C"
