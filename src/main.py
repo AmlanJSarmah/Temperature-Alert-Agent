731090f4b1de4638ae9ded7d766aaf50a9287d6f
A-J-Sarmah <ajsarmah@tutanota.com>
2023-10-10 13:47:56 +0530

    Added relevant comments

--- src/main.py
+++ src/main.py
@@ -9,21 +9,26 @@ if __name__ == "__main__":
     temperature_min = int(input("Enter minimum temperature "))
     temperature_max = int(input("Enter maximum temperature "))
 
+    # Check if minimum temperature is greater than maximum temperature
     if temperature_min >= temperature_max:
         print("ERROR : Minimum temperature cannot be greater than or equal to maximum temperature")
         exit()
 
     temperature_alert_agent = Agent(name="temperature_alert_agent")
 
-
+    # Periodically checks if the temperature exceeded the threshold and notify the user
     @temperature_alert_agent.on_interval(period=3.0)
     async def check_temperature_range(ctx: Context):
+        # Collects the current temperature of given city
         city_temperature = get_city_temperature(city_name, temperature_unit)
+        # Check if given temperature exceeds threshold
         if city_temperature is not None:
             if city_temperature > temperature_max:
-                ctx.logger.info(f"The Temperature of {city_name} is {city_temperature} which is more than {temperature_max}")
+                ctx.logger.info(
+                    f"The Temperature of {city_name} is {city_temperature} which is more than {temperature_max}")
             elif city_temperature < temperature_min:
-                ctx.logger.info(f"The Temperature of {city_name} is {city_temperature} which is less than {temperature_min}")
+                ctx.logger.info(
+                    f"The Temperature of {city_name} is {city_temperature} which is less than {temperature_min}")
             else:
                 pass
         else:
